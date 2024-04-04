0ca0b345d08c057630f436022f46c6eaf45a76c2
Srihari Lakshminarasimhan <v-sriharil@microsoft.com>
2024-04-04 10:58:21 +0530

    Model  Validations

--- Data/Books.cs
+++ Data/Books.cs
@@ -1,8 +1,12 @@
-﻿namespace BookStore.API.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.Data
 {
     public class Books
     {
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
 
